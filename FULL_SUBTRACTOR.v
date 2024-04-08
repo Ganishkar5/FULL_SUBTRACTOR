e47cff591a5aa6167d0dc6417161149e7b3e9220
Ganishkar5 <166374831+Ganishkar5@users.noreply.github.com>
2024-04-08 14:05:34 +0530

    Update FULL_SUBTRACTOR.v

--- FULL_SUBTRACTOR.v
+++ FULL_SUBTRACTOR.v
@@ -20,9 +20,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module full_subtractor(a, b, c,D, Bout);
-input a, b, c;
-output D, Bout;
-                               //Write the logic for full adder in Dataflow modeling
-  
-endmodule  
+ module full_subtractor(
+    input A, // Minuend
+    input B, // Subtrahend
+    input Cin, // Borrow input
+    output Diff, // Difference
+    output Bout // Borrow output
+    );
+
+    wire D1, B1, B2;
+    
+    // First half subtractor
+    half_subtractor HS1(.A(A), .B(B), .Diff(D1), .Borrow(B1));
+    
+    // Second half subtractor
+    half_subtractor HS2(.A(D1), .B(Cin), .Diff(Diff), .Borrow(B2));
+    
+    // OR gate to compute final borrow output
+    assign Bout = B1 | B2;
+    endmodule
